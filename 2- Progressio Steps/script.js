17b7713945ff31d781657da091ee8d7c6b30a4d0
gpswazir <gpswazir@gmail.com>
2024-12-16 19:03:12 +0530

    name 2

--- 2- Progressio Steps/script.js
+++ 2- Progressio Steps/script.js
@@ -3,58 +3,53 @@ let preButn = document.getElementById('preview');
 let line = document.querySelector('.line');
 let circle = document.querySelectorAll('.num');
 
-
-
 nextbutn.addEventListener("click", lineMaker);
 preButn.addEventListener("click", circleMaker);
 
-let sum =1;
-
+let sum = 1;
 
 function lineMaker(eventDetails) {
     sum++
 
-    if(sum > circle.length){
+    if (sum > circle.length) {
         sum = circle.length;
     }
-  
+
     console.log("hello")
-update();
+    update();
 }
 
 function circleMaker(eventDetails) {
     sum--
-    if(sum < 1){
-        sum = 1 ;
+    if (sum < 1) {
+        sum = 1;
     }
-    
-update();
+
+    update();
 }
- 
 
-
-function update(){
-    circle.forEach((circle,index) => {
-        if(index < sum){
+function update() {
+    circle.forEach((circle, index) => {
+        if (index < sum) {
             circle.classList.add("active");
-            }  else{
+        } else {
             circle.classList.remove("active");
-        }           
-    
+        }
+
     }
-);
+    );
 
-const active = document.querySelectorAll(".active");
+    const active = document.querySelectorAll(".active");
 
-line.style.width = (active.length -1) / (circle.length -1) * 100 + '%';
+    line.style.width = (active.length - 1) / (circle.length - 1) * 100 + '%';
 
-if(sum == 1){
-    preButn.disabled = true;
-} else if(sum === circle.length){
-    nextbutn.disabled = true;
-} else{
-    preButn.disabled = false;
-    nextbutn.disabled = false;
+    if (sum == 1) {
+        preButn.disabled = true;
+    } else if (sum === circle.length) {
+        nextbutn.disabled = true;
+    } else {
+        preButn.disabled = false;
+        nextbutn.disabled = false;
 
-}
+    }
 }
